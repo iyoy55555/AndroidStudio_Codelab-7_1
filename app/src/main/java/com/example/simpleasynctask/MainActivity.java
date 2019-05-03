97acc784cb6033375c0a6eabd41aad4a2fae0c3b
陳暉元 <chenhuiyuan@chenhuiyuande-MacBook-Pro.local>
2019-05-04 01:25:53 +0800

    Version 1

---
+++ app/src/main/java/com/example/simpleasynctask/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.simpleasynctask;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // The TextView where we will show results
+    private TextView mTextView;
+    private static final String TEXT_STATE = "currentText";
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if(savedInstanceState!=null)
+            mTextView.setText(savedInstanceState.getString(TEXT_STATE));
+        mTextView = findViewById(R.id.textView1);
+    }
+
+    public void startTask(View view) {
+        // Put a message in the text view
+        mTextView.setText(R.string.napping);
+
+        // Start the AsyncTask.
+        new SimpleAsyncTask(mTextView).execute();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // Save the state of the TextView
+        outState.putString(TEXT_STATE,
+                mTextView.getText().toString());
+    }
+}
+}
